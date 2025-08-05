0662baa74959424d892654fecae4029d5612548d
Mena599 <20243rd0120@utez.edu.mx>
2025-08-05 09:11:43 -0600

    se agregaron las librerias correspondientes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,7 @@
 module org.example.crud {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.bootstrapfx.core;
-
+    requires java.sql;
     opens org.example.crud to javafx.fxml;
     exports org.example.crud;
 }
--- src/main/java/org/example/crud/HelloController.java
+++ src/main/java/org/example/crud/HelloController.java
@@ -1,7 +1,17 @@
 package org.example.crud;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import javax.swing.text.TableView;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class HelloController {
     @FXML
